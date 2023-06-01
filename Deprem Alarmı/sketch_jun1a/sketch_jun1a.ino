f22dd09cb8b244b25baf784202c4153138c1102a
BatikanEge <jfd6tcivoy@gmail.com>
2023-06-01 18:12:09 +0300

    Deprem Alarmı kod

---
+++ Deprem Alarmı/sketch_jun1a/sketch_jun1a.ino
@@ -0,0 +1,22 @@
+#include <MPU6050.h>
+#include <Wire.h>
+MPU6050 MPU;
+int GyroX , GyroY , GyroZ;
+int buzzer = 10;
+void setup() {
+  pinMode(11,OUTPUT); 
+  Serial.begin(9600);
+  Wire.begin();
+  MPU.initialize();
+}
+void loop() {
+  MPU.getRotation(&GyroX, &GyroY, &GyroZ);
+  if(GyroX< -1000 ||GyroX> 1000 || GyroY>1000 || GyroY< -1000 || GyroZ >1000 || GyroZ < -1000 ) {
+  tone(buzzer,1000);
+  digitalWrite(11,HIGH);
+  delay(1000);
+  } else {
+  noTone(buzzer);
+  digitalWrite(11,LOW);
+  }
+}
